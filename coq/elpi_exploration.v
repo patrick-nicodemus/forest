1201e4ff195fc62d83ebaa8a14c2006ee0181e19
Patrick Nicodemus <gadget142@gmail.com>
2024-08-25 21:22:59 -0400

    Update.

--- coq/elpi_exploration.v
+++ coq/elpi_exploration.v
@@ -147,7 +147,8 @@ Goal adef.
 Elpi Tactic rewrite1.
 
 (** Eq is a constant from the global environment whose type is an equality proof 
-    [a = b]. Rewrite all occurrences of [b] to [a], going from right to left. *)
+    [a = b]. Rewrite all occurrences of [b] to [a], going from right to left. 
+    Note that all the rewrite examples currently use Paulin-Mohring equality.  *)
 
 Elpi Accumulate lp:{{
     solve (goal _ _ GoalType _ [trm Eq] as G) GL :-
@@ -185,13 +186,12 @@ Proof.
     Abort.
 
 Elpi Tactic rewrite2.
-
 (** Argument is the name of a hypothesis in the context,
     whose type is of the form forall (x : A), P = Q *)
 Elpi Accumulate lp:{{
     solve (goal Ctx _ GoalType _ [trm Eq] as G) GL :-
         std.mem Ctx (decl Eq _ Ty),
-        Ty = prod _ A (c\ {{ @eq lp:S lp:{{ Q0_ c}} lp:{{Q c}} }}),
+        Ty = prod _ A (c\ {{ @eq lp:S lp:{{ Q0 c}} lp:{{Q c}} }}),
         Hole = {{ _ }}, 
         coq.typecheck Hole A ok,
         pi x\ (copy J x :- 
@@ -201,7 +201,7 @@ Elpi Accumulate lp:{{
             (refine (match 
                 (app [Eq, Hole])
                 (fun _ S (a\   
-                   fun _ {{ @eq lp:S lp:{{ Q0_ Hole}} lp:{{Q Hole}} }} (_\
+                   fun _ {{ @eq lp:S lp:{{ Q0 Hole}} lp:{{Q Hole}} }} (_\
                      P a
                 )))
         [{{_}}]) G GL 
@@ -220,6 +220,83 @@ Proof.
 
     (** Goal is [2 * (1 + (x + x)) = (1 + (x + x)) * 2] *)
     Abort.
+
+(** An interesting bug I hit while 
+    working through this next one: Elpi usually parses
+    the name of a global reference as a global reference.
+    But if you have implicit arguments enabled, and automatic
+    application turned on, then Coq will automatically
+    apply your term to metavariables before feeding it to Elpi,
+    and once it's applied, it's a term, not a global reference.
+*)
+
+Elpi Tactic rewrite3.
+(** Argument either the name of a hypothesis in the context,
+    or the name of a global definition (variables count as global references
+    as far as Elpi is concerned, although you can get a list of
+    the section-local variables by calling coq.env.section.
+    
+    The declared type of the argument should be type is of the form 
+    [forall (x1 : A1) (x2 : A2) ... P = Q]
+    
+    Unifies Q with a subterm of the goal and rewrites right to left.
+*)
+Elpi Accumulate lp:{{
+    pred nested_forall i:term, i:term, o:goal, o:list sealed-goal.
+    nested_forall Eqpf {{@eq lp:S lp:P lp:Q }} (goal _ _ GoalType _ _ as G) GL :- 
+        pi x\ (copy J x :- coq.unify-leq Q J ok) =>
+            copy GoalType (A x),
+            if (occurs x (A x))
+            (refine (match
+                Eqpf 
+                (fun _ S (a\
+                   fun _ {{ @eq lp:S lp:P lp:Q }} (_\ A a )
+                   ))
+                % We only want to create one hole,
+                % the one corresponding to the 
+                % contents of the (single) branch of the match.
+                [{{_}}])
+                G GL 
+            )
+            (coq.ltac.fail _ "Couldn't unify.").
+
+    nested_forall Eqpf (prod _ A B) G GL :-
+        Hole = {{ _ }},
+        coq.typecheck Hole A ok,
+        % This might not make progress if
+        % Eqpf is opaque or an assumption.
+        whd (app [Eqpf, Hole]) [] HD ARGS,
+        unwind HD ARGS Eqpf',
+        nested_forall Eqpf' (B Hole) G GL.
+
+    solve (goal Ctx _ _ _ [trm Eq] as G) GL :- (
+        % Eq is a direct reference to a global definition or axiom
+        Eq = global Gref, coq.env.typeof Gref Ty;
+        % Eq is a direct Gallina term and we will infer its type
+        % from context
+        coq.typecheck Eq Ty ok;
+        % Eq is a reference to a declared variable in the context
+        std.mem Ctx (decl Eq _ Ty)),
+        nested_forall Eq Ty G GL.
+}}.
+Elpi Typecheck.
+
+Section Example_rewrite3.
+Variable A : Type.
+Variable B : A -> Type.
+Variable C : forall (a : A) (b : B a), Type.
+Variable add : forall {a : A} {b : B a}, C a b -> C a b -> C a b.
+Variable sym : forall {a : A} {b : B a} (c c' : C a b), add c c' = add c' c.
+
+Goal forall (a : A) (b : B a) (x y : C a b),
+    add x y = add y x /\ add x y = add y x.
+Proof.
+    intros a b x y.
+    elpi rewrite3 (@sym).
+    (** [add y x = add y x /\ add y x = add y x] *)
+    Abort.
+
+End Example_rewrite3.
 (** Okay. I still need more practice with 
 Elpi but this is a good start. 
 We will have to continue and extend the rewrite example, and add other variations.
